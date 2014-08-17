76ede2a3c2ccbf3ec4dadba995573421e46469e9
zelzel2315 <criselda.valderrama@gmail.com>
2014-08-17 13:48:14 -0700

    made a root, please work now

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ MUSG::Application.routes.draw do
   end
 
  # get 'true_shades/home' => 'true_shades#home', as: :home
-  root 'session#new'
+  root 'users#new'
 
   resource :session, only: [:new, :create, :destroy]
 
